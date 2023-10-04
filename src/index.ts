1f9edf4bfa11ffabc1f94b96dfb4ed12e7cc841c
JuhLima85 <limajuh17@gmail.com>
2023-10-04 16:05:30 -0300

    CRUD de usuários

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,14 @@
-/*
-import express from 'express';
-import statusRoute from './routes/status.route';
-import usersRoute from './routes/users.route';
+import express, { Request, Response, NextFunction } from "express";
+import usersRoute from "./routes/users.route";
+import statusRoute from "./routes/status.route";
 
 const app = express();
 
-// Configurações da aplicação
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Configurações de Rotas
-app.use(statusRoute);
 app.use(usersRoute);
-
-// Inicialização do servidor
-app.listen(3000, () => {
-    console.log('Aplicação executando na porta 3000!');
-});*/
-
-import express, { Request, Response, NextFunction } from "express";
-
-const app = express();
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({foo: 'bar'});
-})
+app.use(statusRoute)
 
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000.')
